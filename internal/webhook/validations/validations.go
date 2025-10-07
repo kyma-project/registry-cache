7359af2eaa987c00feb247b6bf3f3545d6afb15f
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2025-10-07 09:34:38 +0200

    Started implementing validator

--- internal/webhook/validations/validations.go
+++ internal/webhook/validations/validations.go
@@ -1,7 +1,8 @@
 package validations
 
 import (
-	registrycacheext "github.com/gardener/gardener-extension-registry-cache/pkg/apis/registry/v1alpha3"
+	registrycacheext "github.com/gardener/gardener-extension-registry-cache/pkg/apis/registry"
+	registrycacheextvalidations "github.com/gardener/gardener-extension-registry-cache/pkg/apis/registry/validation"
 	registrycache "github.com/kyma-project/registry-cache/api/v1beta1"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -21,15 +22,18 @@ func NewValidator(secrets []v1.Secret, existingConfigs []registrycache.RegistryC
 }
 
 func (v Validator) Do(newConfig *registrycache.RegistryCacheConfig) field.ErrorList {
-	return nil
+	objectToValidate := toExtensionConfig(*newConfig)
+
+	return registrycacheextvalidations.ValidateRegistryConfig(objectToValidate, field.NewPath("spec"))
 }
 
 func (v Validator) DoOnUpdate(newConfig, oldConfig *registrycache.RegistryCacheConfig) field.ErrorList {
 	return nil
 }
 
-func toExtensionConfig(rc registrycache.RegistryCacheConfig) registrycacheext.RegistryConfig {
-	return registrycacheext.RegistryConfig{
+func toExtensionConfig(rc registrycache.RegistryCacheConfig) *registrycacheext.RegistryConfig {
+
+	return &registrycacheext.RegistryConfig{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "registry.extensions.gardener.cloud/v1alpha3",
 			Kind:       "RegistryConfig",
@@ -38,6 +42,34 @@ func toExtensionConfig(rc registrycache.RegistryCacheConfig) registrycacheext.Re
 	}
 }
 
-func toExtensionCache(registryCacheConfig registrycache.RegistryCacheConfigSpec) registrycacheext.RegistryCache {
-	return registrycacheext.RegistryCache{}
+func toExtensionCache(c registrycache.RegistryCacheConfigSpec) registrycacheext.RegistryCache {
+	ext := registrycacheext.RegistryCache{
+		Upstream:            c.Upstream,
+		RemoteURL:           c.RemoteURL,
+		SecretReferenceName: c.SecretReferenceName,
+	}
+	if c.Volume != nil {
+		ext.Volume = &registrycacheext.Volume{
+			Size:             c.Volume.Size,
+			StorageClassName: c.Volume.StorageClassName,
+		}
+	}
+	if c.GarbageCollection != nil {
+		ext.GarbageCollection = &registrycacheext.GarbageCollection{
+			TTL: c.GarbageCollection.TTL,
+		}
+	}
+	if c.Proxy != nil {
+		ext.Proxy = &registrycacheext.Proxy{
+			HTTPProxy:  c.Proxy.HTTPProxy,
+			HTTPSProxy: c.Proxy.HTTPSProxy,
+		}
+	}
+	if c.HTTP != nil {
+		ext.HTTP = &registrycacheext.HTTP{
+			TLS: c.HTTP.TLS,
+		}
+	}
+
+	return ext
 }

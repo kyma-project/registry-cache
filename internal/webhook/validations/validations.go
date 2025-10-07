b709f497d60a56b6c616a7edef9df428b1b4683f
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2025-10-07 09:04:49 +0200

    Started implementing converter from CRD to Gardener extension type

--- internal/webhook/validations/validations.go
+++ internal/webhook/validations/validations.go
@@ -1,8 +1,10 @@
 package validations
 
 import (
+	registrycacheext "github.com/gardener/gardener-extension-registry-cache/pkg/apis/registry/v1alpha3"
 	registrycache "github.com/kyma-project/registry-cache/api/v1beta1"
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 )
 
@@ -24,4 +26,18 @@ func (v Validator) Do(newConfig *registrycache.RegistryCacheConfig) field.ErrorL
 
 func (v Validator) DoOnUpdate(newConfig, oldConfig *registrycache.RegistryCacheConfig) field.ErrorList {
 	return nil
+}
+
+func toExtensionConfig(rc registrycache.RegistryCacheConfig) registrycacheext.RegistryConfig {
+	return registrycacheext.RegistryConfig{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "registry.extensions.gardener.cloud/v1alpha3",
+			Kind:       "RegistryConfig",
+		},
+		Caches: []registrycacheext.RegistryCache{toExtensionCache(rc.Spec)},
+	}
+}
+
+func toExtensionCache(registryCacheConfig registrycache.RegistryCacheConfigSpec) registrycacheext.RegistryCache {
+	return registrycacheext.RegistryCache{}
 }

05cc62477543407a173de3c57e82e94078de6154
IwonaLanger <iwona.langer@sap.com>
2026-07-22 13:09:10 +0200

    Update the sidebar file

--- docs/user/_sidebar.ts
+++ docs/user/_sidebar.ts
@@ -1,24 +1,10 @@
-// TODO: Add the docs/user/README.md entry to the overarching sidebar in the kyma repo.
 export default [
-  { text: 'Configure Registry Cache', link: './01-10-configure-registry-cache.md' },
-  {
-    text: 'Resources',
-    link: './resources/README.md',
-    collapsed: true,
-    items: [
-      { text: 'RegistryCache', link: './resources/RegistryCache.md' },
-      { text: 'RegistryCacheConfig', link: './resources/RegistryCacheConfig.md' },
-    ],
-  },
-  {
-    text: 'Troubleshooting',
-    link: './troubleshooting/README.md',
-    collapsed: true,
-    items: [
-      {
-        text: 'Registry Cache Does Not Cache Images from Private Registry',
-        link: './troubleshooting/01-10-incorrect-credentials.md',
-      },
-    ],
-  },
+  { text: 'Configure Registry Cache', link: './01-10-configure-registry-cache' },
+  { text: 'Resources', link: './resources/README', collapsed: true, items: [
+    { text: 'RegistryCache', link: './resources/RegistryCache' },
+    { text: 'RegistryCacheConfig', link: './resources/RegistryCacheConfig' },
+    ] },
+  { text: 'Troubleshooting', link: './troubleshooting/README', collapsed: true, items: [
+    { text: 'Registry Cache Does Not Cache Images from Private Registry', link: './troubleshooting/01-10-incorrect-credentials' },
+    ] }
 ];
